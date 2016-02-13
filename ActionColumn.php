5faecef165f2a9a0b052f327f1605c0606d301e5
Vadim Mazur <mazurva@gmail.com>
2016-02-13 15:58:15 +0600

    Update

--- ActionColumn.php
+++ ActionColumn.php
@@ -99,6 +99,19 @@ class ActionColumn extends \yii\grid\ActionColumn
         }
     }
 
+    public function registerTranslations()
+    {
+        $i18n = \Yii::$app->i18n;
+        $i18n->translations['sibds/grid/*'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => 'en-US',
+            'basePath' => '@vendor/sibds/yii2-gridhelper/messages',
+            'fileMap' => [
+                'sibds/grid/messages' => 'messages.php',
+            ],
+        ];
+    }
+
     public static function t($category, $message, $params = [], $language = null)
     {
         return \Yii::t('sibds/grid/' . $category, $message, $params, $language);
