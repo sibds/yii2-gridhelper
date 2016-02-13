de9d38cc24667a10654527d70074af8b7ae01897
Vadim Mazur <mazurva@gmail.com>
2016-02-13 15:43:44 +0600

    Update

--- ActionColumn.php
+++ ActionColumn.php
@@ -9,7 +9,71 @@
 namespace sibds\grid;
 
 
-class ActionColumn
-{
+use kartik\icons\Icon;
 
+class ActionColumn extends \yii\grid\ActionColumn
+{
+    public $template = '{update} {copy} {lock}{unlock} {restore}{delete}';
+
+    /**
+     * Initializes the default button rendering callbacks.
+     */
+    protected function initDefaultButtons()
+    {
+        /* TODO: Add support!
+        if (!isset($this->buttons['view'])) {
+            $this->buttons['view'] = function ($url, $model, $key) {
+                $options = array_merge([
+                    'title' => Yii::t('yii', 'View'),
+                    'aria-label' => Yii::t('yii', 'View'),
+                    'data-pjax' => '0',
+                ], $this->buttonOptions);
+                return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, $options);
+            };
+        }
+        */
+        if (!isset($this->buttons['update'])) {
+            $this->buttons['update'] = function ($url, $model, $key) {
+                $options = array_merge([
+                    'title' => Yii::t('message', 'Update'),
+                    'aria-label' => Yii::t('message', 'Update'),
+                    'data-pjax' => '0',
+                ], $this->buttonOptions);
+                return Html::a(Icon::show('pencil', [], Icon::FA), $url, $options);
+            };
+        }
+        if (!isset($this->buttons['copy'])) {
+            $this->buttons['copy'] = function ($url, $model, $key) {
+                $options = array_merge([
+                    'title' => Yii::t('message', 'Copy'),
+                    'aria-label' => Yii::t('message', 'Copy'),
+                    'data-pjax' => '0',
+                ], $this->buttonOptions);
+                return Html::a(Icon::show('copy', [], Icon::FA), $url, $options);
+            };
+        }
+        if (!isset($this->buttons['lock'])) {
+            $this->buttons['lock'] = function ($url, $model, $key) {
+                $options = array_merge([
+                    'title' => Yii::t('message', 'Update'),
+                    'aria-label' => Yii::t('message', 'Update'),
+                    'data-pjax' => '0',
+                ], $this->buttonOptions);
+                return Html::a(Icon::show('copy', [], Icon::FA), $url, $options);
+            };
+        }
+        if (!isset($this->buttons['delete'])) {
+            $this->buttons['delete'] = function ($url, $model, $key) {
+                $options = array_merge([
+                    'title' => Yii::t('message', 'Delete'),
+                    'aria-label' => Yii::t('message', 'Delete'),
+                    'data-confirm' => Yii::t('message', 'Are you sure you want to delete this item?'),
+                    'data-method' => 'post',
+                    'data-pjax' => '0',
+                ], $this->buttonOptions);
+
+                return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, $options);
+            };
+        }
+    }
 }
---
+++ messages/ru/messages.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vadim
+ * Date: 27.01.16
+ * Time: 1:40
+ */
+return [
+    'Edit' => 'Редактировать',
+    'Copy' => 'Копировать',
+    'Lock'=>'Заблокировать',
+    'Unlock'=>'Разблокировать',
+    'To trash'=>'В корзину',
+    'Restore'=>'Восстановить',
+    'Delete'=>'Удалить',
+    'Are you sure you want to delete this item?' => 'Вы действительно хотите удалить этот элемент?',
+];
